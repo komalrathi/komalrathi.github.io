d7ede98b6fed7781433935be928e1f7f698a3f33
Komal Rathi <52624801+komalrathi@users.noreply.github.com>
2021-10-30 22:49:13 +0100

    importing fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import App from './home'
+import "@fontsource/concert-one"
+import "@fontsource/lora"
+import "@fontsource/andada-pro"
+
+import "@fontsource/josefin-sans"
+import "@fontsource/comfortaa"
+import "@fontsource/biorhyme"
+import "@fontsource/playfair-display"
 
 export default function Webpage() {
     return (
