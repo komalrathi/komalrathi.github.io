da921a72622061ab4bc50931affac4b2bca07e86
Komal Rathi <52624801+komalrathi@users.noreply.github.com>
2021-11-11 18:15:12 +0000

    added google analytics

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import '../components/App.css';
 import HomePage from '../components/HomePage/HomePage'
 
+
 function App() {
   return (
     <div className="App">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import App from './home'
+import App from './App'
 import "@fontsource/concert-one"
 import "@fontsource/lora"
 import "@fontsource/andada-pro"
@@ -8,6 +8,12 @@ import "@fontsource/josefin-sans"
 import "@fontsource/comfortaa"
 import "@fontsource/biorhyme"
 import "@fontsource/playfair-display"
+
+
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-212617990-1";
+ReactGA.initialize(TRACKING_ID);
+
 
 export default function Webpage() {
     return (
